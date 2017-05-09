c6cdd4f78f7ca6913df154ce19e140222dcc18b6
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-09 19:08:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #1. create new hash
+   new_hash = {}
+   #2.loop through the facts with the values of those facts
+   data.each do |facts, values|
+     #3.loop through the values with the pigeon names
+     values.each do |value, pigeon_name|
+       #4.loop through the array of names and push them into the new hash (the value to string)
+       pigeon_name.each do |pigeon|
+         new_hash[pigeon] ||= {}
+         new_hash[pigeon][facts] ||= []
+         new_hash[pigeon][facts] << value.to_s
+       end
+     end
+   end
+   new_hash
 end
